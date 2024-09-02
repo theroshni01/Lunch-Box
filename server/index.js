30a80a7dacff3f1d1b299f42b5aa93561a57324f
Roshni Syed <105354040+theroshni01@users.noreply.github.com>
2024-09-02 15:00:49 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,14 +17,14 @@ app.use(cors({
     credentials: true
 }));
 
-  app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
+  // app.use(function (req, res, next) {
+  //   //Enabling CORS
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+  //   Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  //     next();
+  //   });
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
