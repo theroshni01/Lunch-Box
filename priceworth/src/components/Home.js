329b0f5b909f470b56bd16914ee49788efedccc6
Roshni Syed <105354040+theroshni01@users.noreply.github.com>
2024-09-03 12:44:05 +0530

    Update Home.js

--- priceworth/src/components/Home.js
+++ priceworth/src/components/Home.js
@@ -19,11 +19,10 @@ function Home( {cartItems, setCartItems}) {
   const [plans, setPlans] = useState([]);
 
     useEffect(() =>{
-        fetch(process.env.REACT_APP_URL+"/plans" )
-		
-	      // {
-		     //  mode : 'no-cors'
-	      // }
+        fetch(process.env.REACT_APP_URL+"/plans" ,
+	      {
+		      mode : 'no-cors'
+	      })
         // .then(res => res.json())
         .then(res => setPlans(res.plans))
     })
